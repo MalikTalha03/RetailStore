7ae0667e4b67dd5df198bb6152a67a498891d62d
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2023-12-28 13:54:13 +0500

    Update imports and add NewOrder component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NewOrder from './components/Orders/NewOrder';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/neworder" element={<NewOrder />} />
       </Routes>
     </BrowserRouter>
   );
