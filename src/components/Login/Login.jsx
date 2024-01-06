00ab95435b3bcfa4cc852c0e783ef21fd23e3dad
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-06 14:37:16 +0500

    Add login functionality to Login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,14 +3,45 @@ import './login.css'
 import { Link } from 'react-router-dom'
 
 
+
 const Login = () => {
+  const apiurl = "https://retail-store-backend.vercel.app/auth/login"
+  function handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const username = data.get('username')
+    const password = data.get('password')
+    const user = {
+      username,
+      password
+    }
+    fetch(apiurl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      const message = data.message
+      if(message === 'Login Successful') {
+        window.location.href = '/neworder'
+        localStorage.setItem('token', data.token)
+      } else {
+        alert(message)
+      } 
+    })
+    .catch(err => console.log(err))
+
+  }
   return (
     <div>
       <div className='login'>
           <h1>Login</h1>
-          <form>
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" />
+          <form onSubmit={handleSubmit}>
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" name="username" />
               <label htmlFor="password">Password</label>
               <input type="password" id="password" name="password" />
               <button type="submit" >Login</button>
