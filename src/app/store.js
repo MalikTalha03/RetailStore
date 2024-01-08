63e23ae5e9d023e8dfb74390d3d396155268e15b
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-08 23:14:01 +0500

    Add feature slices to store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productsSlice } from "./features/products";
+import { suppliersSlice } from "./features/supplier";
+import { dialogSlice } from "./features/dialogslice";
+import { orderdataSlice } from "./features/orderdata";
+import { categoriesSlice } from "./features/categories";
 
 export const store = configureStore({
     reducer: {
-        // counter: counterReducer,
+        products: productsSlice.reducer,
+        suppliers: suppliersSlice.reducer,
+        dialog: dialogSlice.reducer,
+        orderdata: orderdataSlice.reducer,
+        categories: categoriesSlice.reducer
     },
 });
 
