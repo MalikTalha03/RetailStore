7fab76a2d5d5372edaca6b9633d7d6e1d1ab899e
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-10 09:46:07 +0500

    Add ConfirmationDialog component for order confirmation

---
+++ src/components/Orders/ConfirmDialog.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { Dialog, DialogContent, DialogTitle, DialogContentText, DialogActions, Button } from '@mui/material'
+import '../Supplier/order.css'
+import '../Orders/neworder.css'
+import { useSelector } from 'react-redux'
+
+
+const ConfirmationDialog = ({open, onClose, tabledata, totalPrice}) => {
+    const selectedCustomer = useSelector(state => state.customers.selectedCustomer);
+    const orderData = useSelector(state => state.orderdata.orderdata);
+    const customerid = selectedCustomer && selectedCustomer.length > 0 ? selectedCustomer[0]._id : null;
+    const token = localStorage.getItem('token');
+    const api = 'http://localhost:3001/'
+    async function addOrder(){
+        try{
+            const data = await fetch(api +`customer/${customerid}/orders`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    orderDate: new Date(),
+                    paymentStatus: 'Pending',
+                })
+            })
+            const response = await data.json()
+            const orderid = response.id
+            console.log(response)
+            if(response.message === 'jwt expired'){
+                localStorage.removeItem('token')
+                window.location.reload()
+            }
+            else{
+                try{
+                    orderData.forEach(async (item) => {
+                        console.log(item)
+                        const data = await fetch(api +`customer/${customerid}/orders/${orderid}/details`, {
+                            method: 'PATCH',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': `Bearer ${token}`
+                            },
+                            body: JSON.stringify({
+                                productid: item.id,
+                                qty: item.quantity,
+                                unitPrice: item.price
+                            })
+                        })
+                        const response = await data.json()
+                        if(response.message === 'jwt expired'){
+                            localStorage.removeItem('token')
+                            window.location.reload()
+                        }
+                        else{
+                            alert(response.message)
+                            window.location.reload()
+                        }
+                    })
+                }
+                catch(err){
+                    console.log(err)
+                }
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+  return (
+    <div>
+        <Dialog open={open} onClose={onClose} fullWidth={true} maxWidth='md' >
+                    <DialogTitle>Confirm Order</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            <table className='table'>
+                                <thead>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {tabledata}
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td className='border'><b>Total</b></td>
+                                        <td className='border'>{totalPrice}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </DialogContentText>
+                        <DialogActions>
+                            <Button onClick={onClose}>Cancel</Button>
+                            <Button onClick={addOrder}>Add</Button>
+                        </DialogActions>
+                    </DialogContent>
+                </Dialog>
+    </div>
+  )
+}
+
+export default ConfirmationDialog
