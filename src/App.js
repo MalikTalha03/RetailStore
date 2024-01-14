35114f866125f6c1fd16279c196532ce999210e1
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-14 17:04:47 +0500

    Remove Navbar component and add Drawer component to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NewOrder from './components/Orders/NewOrder';
 import OrderDetails from './components/Orders/OrderDetails';
 import SuppOrder from './components/Supplier/SuppOrder';
+import Drawer from './components/Dashboard/Drawer';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
+        <Route path="/" element={<Drawer />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/neworder" element={<NewOrder />} />
