28b63e105eba32f93e6bad2e0ac1ebc4f5eea880
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-18 12:42:44 +0500

    Refactor SupplierPayment component

--- src/components/Supplier/SupplierPayment.jsx
+++ src/components/Supplier/SupplierPayment.jsx
@@ -1,44 +1,57 @@
-import React, { useState } from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchSuppOrders } from '../../app/features/supplier';
-import { useEffect } from 'react';
-import { makeStyles } from '@mui/styles';
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchSuppOrders } from "../../app/features/supplier";
+import { useEffect } from "react";
+import checkToken from "../loggedin";
 
-const SupplierPayment = () => {
+const SupplierPayment = ({ open, onClose }) => {
+  checkToken();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchSuppOrders());
   }, [dispatch]);
   const suppliers = useSelector((state) => state.suppliers.supplierOrders);
-  console.log(suppliers);
   const [selectedSupplier, setSelectedSupplier] = useState(null);
-  const [selectedOrder, setSelectedOrder] = useState(''); // Initialize with an empty string
+  const [selectedOrder, setSelectedOrder] = useState("");
   const [paymentAmount, setPaymentAmount] = useState(0);
   const [remainingAmount, setRemainingAmount] = useState(0);
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
 
   const handleSupplierChange = (event) => {
-    const selectedSupplier = suppliers.find((supplier) => supplier.name === event.target.value);
+    const selectedSupplier = suppliers.find(
+      (supplier) => supplier.name === event.target.value
+    );
     setSelectedSupplier(selectedSupplier);
-    setSelectedOrder(''); // Reset selected order and amounts when the supplier changes
+    setSelectedOrder("");
     setPaymentAmount(0);
     setRemainingAmount(0);
   };
 
   const handleOrderChange = (event) => {
-    const selectedOrder = selectedSupplier.orders.find((order) => order._id === event.target.value);
+    const selectedOrder = selectedSupplier.orders.find(
+      (order) => order._id === event.target.value
+    );
     setSelectedOrder(selectedOrder);
-    setPaymentAmount(0); // Reset payment amount when the order changes
-    setRemainingAmount(selectedOrder.totalAmount);
+    setPaymentAmount(0);
+    const rem =
+      selectedOrder.totalAmount -
+      selectedOrder.transactions.reduce(
+        (acc, curr) => acc + curr.totalAmount,
+        0
+      );
+    setRemainingAmount(rem);
   };
 
   const handlePaymentAmountChange = (event) => {
@@ -48,52 +61,52 @@ const SupplierPayment = () => {
   };
 
   const paySupplier = async () => {
-    let status = ''
+    let status = "";
     const url = `http://localhost:3001/supplier/${selectedSupplier._id}/orders/${selectedOrder._id}/transactions`;
 
     const data = await fetch(url, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('token'),
-        },
-        body: JSON.stringify({
-            transactionDate: new Date(),
-            transactionType: 'Cash',
-            totalAmount: paymentAmount,
-        }),
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      body: JSON.stringify({
+        transactionDate: new Date(),
+        transactionType: "Cash",
+        totalAmount: paymentAmount,
+      }),
     });
     status = data.status;
     const response = await data.json();
-    if (response.message === 'jwt expired') {
-        localStorage.removeItem('token');
-        window.location.reload();
+    if (response.message === "jwt expired") {
+      localStorage.removeItem("token");
+      window.location.reload();
     }
-    console.log(response)
+    console.log(response);
     if (status === 200) {
-        alert('Payment Successful!');
+      alert("Payment Successful : " + response.message);
+      onClose();
+      setPaymentAmount(0);
+      setRemainingAmount(0);
+      setSelectedOrder("");
+      setSelectedSupplier(null);
     } else {
-        alert('Payment Failed!');
-
+      alert("Payment Failed! : " + response.message);
     }
-    handleClose();
   };
 
   return (
     <>
-      <Button variant="contained" onClick={handleOpen} style={{ padding: '5rem' }}>
-        Pay Supplier
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={open} onClose={onClose}>
         <DialogTitle>{`Pay Supplier`}</DialogTitle>
         <DialogContent>
           <Typography variant="body1">Select the supplier:</Typography>
-          <FormControl fullWidth style={{ marginTop: '1rem'}}>
+          <FormControl fullWidth style={{ marginTop: "1rem" }}>
             <InputLabel id="supplier-select-label">Supplier</InputLabel>
             <Select
               labelId="supplier-select-label"
               id="supplier-select"
-              value={selectedSupplier ? selectedSupplier.name : ''}
+              value={selectedSupplier ? selectedSupplier.name : ""}
               onChange={handleSupplierChange}
               label="Supplier"
             >
@@ -107,13 +120,15 @@ const SupplierPayment = () => {
 
           {selectedSupplier && (
             <>
-              <Typography variant="body1" style={{ marginTop: '2rem'}}>Select the order to pay:</Typography>
-              <FormControl fullWidth style={{ marginTop: '1rem'}}>
+              <Typography variant="body1" style={{ marginTop: "2rem" }}>
+                Select the order to pay:
+              </Typography>
+              <FormControl fullWidth style={{ marginTop: "1rem" }}>
                 <InputLabel id="order-select-label">Order</InputLabel>
                 <Select
                   labelId="order-select-label"
                   id="order-select"
-                  value={selectedOrder ? selectedOrder._id : ''}
+                  value={selectedOrder ? selectedOrder._id : ""}
                   onChange={handleOrderChange}
                   label="Order"
                 >
@@ -125,9 +140,11 @@ const SupplierPayment = () => {
                 </Select>
               </FormControl>
 
-              <Typography variant="body1" style={{ marginTop: '2rem'}}>Enter the payment amount:</Typography>
+              <Typography variant="body1" style={{ marginTop: "2rem" }}>
+                Enter the payment amount:
+              </Typography>
               <TextField
-                style={{ marginTop: '1rem'}}
+                style={{ marginTop: "1rem" }}
                 id="payment-amount"
                 label="Payment Amount"
                 type="number"
@@ -137,15 +154,21 @@ const SupplierPayment = () => {
                 fullWidth
               />
 
-              <Typography variant="body1">Remaining Amount: {remainingAmount}</Typography>
+              <Typography variant="body1">
+                Remaining Amount: {remainingAmount}
+              </Typography>
             </>
           )}
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} variant="outlined">
+          <Button onClick={onClose} variant="outlined">
             Cancel
           </Button>
-          <Button onClick={paySupplier} variant="contained" disabled={!selectedSupplier || !selectedOrder || paymentAmount <= 0}>
+          <Button
+            onClick={paySupplier}
+            variant="contained"
+            disabled={!selectedSupplier || !selectedOrder || paymentAmount <= 0}
+          >
             Pay
           </Button>
         </DialogActions>
