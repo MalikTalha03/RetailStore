41719a562c37d89253a66cac8c9cc85877e297ba
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-29 10:30:34 +0500

    Add AddEmployee component

---
+++ src/components/Employee/AddEmployee.jsx
@@ -0,0 +1,160 @@
+// AddEmployee.js
+import React, { useState, useEffect } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+} from "@mui/material";
+
+const AddEmployee = ({ open, onClose }) => {
+  const initialFormData = {
+    firstname: "",
+    lastname: "",
+    contact: "",
+    address: "",
+    salary: "",
+    position: "",
+    password: "",
+    username: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  useEffect(() => {
+    if (open) {
+      setFormData(initialFormData);
+    }
+  }, [open]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}employee`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to add employee");
+      }
+
+      const responseData = await response.json();
+      alert("Employee added successfully");
+      onClose(); 
+    } catch (error) {
+      alert("Error adding employee:", error.message);
+    }
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Add Employee</DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="First Name"
+            type="text"
+            name="firstname"
+            value={formData.firstname}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Last Name"
+            type="text"
+            name="lastname"
+            value={formData.lastname}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Contact"
+            type="text"
+            name="contact"
+            value={formData.contact}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Address"
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Salary"
+            type="text"
+            name="salary"
+            value={formData.salary}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Position"
+            type="text"
+            name="position"
+            value={formData.position}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Password"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Username"
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+          />
+
+          <DialogActions>
+            <Button type="submit" variant="contained" color="primary">
+              Add Employee
+            </Button>
+            <Button onClick={onClose} variant="outlined" color="primary">
+              Cancel
+            </Button>
+          </DialogActions>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddEmployee;
