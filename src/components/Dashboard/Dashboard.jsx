d20c0f6a2ad0de1857d4dbf5069ff82b40eccf96
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-30 20:35:13 +0500

    Refactor Dashboard component and add data fetching
    The changes include refactoring the Dashboard component to use React hooks, adding data fetching for orders, suppliers, products, and customers, and displaying the fetched data in cards on the dashboard.

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
-import checkToken from '../loggedin';
+import React from "react";
+import checkToken from "../loggedin";
+import { fetchTodayOrders } from "../../app/features/orders";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchSuppliers } from "../../app/features/supplier";
+import { fetchProducts } from "../../app/features/products";
+import { fetchCustomers } from "../../app/features/customer";
+import "./navbar.css";
 
 const Dashboard = () => {
-  checkToken();
+  useEffect(() => {
+    checkToken();
+  }, []);
+  const dispatch = useDispatch();
+  const orders = useSelector((state) => state.orders.orders);
+  const suppliers = useSelector((state) => state.suppliers.suppliers);
+  const products = useSelector((state) => state.products.products);
+  const customers = useSelector((state) => state.customers.customers);
+  let tcust = customers.length;
+  let tprod = products.length;
+  let tsup = suppliers.length;
+  let tord = orders.length;
+  useEffect(() => {
+    dispatch(fetchTodayOrders());
+    dispatch(fetchSuppliers());
+    dispatch(fetchProducts());
+    dispatch(fetchCustomers());
+  }, [dispatch]);
   return (
-    <div>
-        <h1>Dashboard</h1>
-
-
+    <div className="cardscontainer">
+      <div className="card">
+        <div className="cardtitle">Today Total Orders</div>
+        <div className="cardvalue">{tord}</div>
+      </div>
+      <div className="card">
+        <div className="cardtitle">Total Products</div>
+        <div className="cardvalue">{tprod}</div>
+      </div>
+      <div className="card">
+        <div className="cardtitle">Total Customers</div>
+        <div className="cardvalue">{tcust}</div>
+      </div>
+      <div className="card">
+        <div className="cardtitle">Total Suppliers</div>
+        <div className="cardvalue">{tsup}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
